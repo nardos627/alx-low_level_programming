3106a9d62cb4b20e5fb635dd32af685879ffd0d1
Nardos <nardoshaddis5@gmail.com>
2023-04-25 00:21:39 -0700

    6-11 tasks

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of integer
+ * @n: integer argument
+ * Return: last digit of number
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+
+	_putchar(last + '0');
+
+	return (last);
+}
